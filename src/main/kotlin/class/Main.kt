e2cdf5bf279a2056564d2fcfeab747d9dd787974
suguru-3u <suguru.13579515@gmail.com>
2025-04-26 09:02:36 +0900

    study class

--- src/main/kotlin/class/Main.kt
+++ src/main/kotlin/class/Main.kt
@@ -2,6 +2,11 @@ package org.example.`class`
 
 import kotlin.random.Random
 
+/**
+ * 公式サイト
+ * https://kotlinlang.org/docs/classes.html
+ */
+
 fun main() {
     val emp = Employee("Mary", 20)
     println(emp)
@@ -15,20 +20,77 @@ fun main() {
     )
     println(person)
 
-    val empGen = RandomEmployeeGenerator(10, 30)
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-    println(empGen.generateEmployee())
-    empGen.minSalary = 50
-    empGen.maxSalary = 100
-    println(empGen.generateEmployee())
+    ShapeFactory.createShape("circle").draw()
 }
 
-class RandomEmployeeGenerator(var maxSalary: Int, var minSalary: Int){
-    private val names = listOf("John", "Mary", "Ann", "Paul", "Jack", "Elizabeth")
+// 以下の条件を満たすKotlinプログラムを作成してください。
+//
+// 1. 抽象クラス「Shape」を作成し、抽象関数「draw」を定義してください。
+// 2. 「Rectangle」と「Circle」という2つのクラスをShapeから継承し、それぞれdraw関数をオーバーライドして、
+//    "Drawing a rectangle"、"Drawing a circle" と出力するようにしてください。
+// 3. Shapeクラスには、共通処理として「name」というプロパティを追加し、主コンストラクタで初期化できるようにしてください。
+// 4. 「ShapeFactory」というクラスを定義し、その中に「createShape」という関数を用意してください。
+//    引数として "rectangle" または "circle" を受け取り、対応するShapeインスタンスを返すようにしてください。
+// 5. ShapeFactoryの「createShape」は、ShapeFactoryのcompanion object内に定義してください。
+// 6. main関数を定義し、ShapeFactory.createShape("circle") のように呼び出して、戻ってきたインスタンスでdraw関数を呼び出してください。
+
+// あなたのコードはこの下に書いてください。
+abstract class Shape(val name: String) {
+    abstract fun draw()
+}
+
+class Rectangle(name: String) : Shape(name) {
+    override fun draw() {
+        println("Drawing a rectangle")
+    }
+}
+
+class Circle(name: String) : Shape(name) {
+    override fun draw() {
+        println("Drawing a circle")
+    }
+}
+
+class ShapeFactory() {
+    companion object {
+        fun createShape(shapeClassName: String): Shape {
+            return when (shapeClassName.lowercase()) {
+                "rectangle" -> Rectangle(shapeClassName)
+                else -> Circle(shapeClassName)
+            }
+        }
+    }
+}
+
+// 以下の条件を満たすKotlinのクラスを作成してください。
+//
+// 1. クラス名は「Book」とする。
+// 2. 「title（タイトル）」「author（著者）」「price（価格）」という3つのプロパティを持つこと。
+// 　　- titleとauthorは読み取り専用（val）、priceは変更可能（var）にすること。
+// 3. 主コンストラクタを使って、すべてのプロパティを初期化すること。
+// 4. priceにはデフォルト値「0」を設定すること。
+// 5. インスタンス生成時に初期化ブロックで「"Book titled '<タイトル>' by <著者> is created."」と出力すること。
+// 6. Bookクラスには、副コンストラクタを1つ追加し、引数として「title」と「author」のみを受け取り、priceは初期値を使用すること。
+
+// あなたのコードはこの下に書いてください。
+
+class Book(val title: String, val author: String, var price: Int = 0) {
+    init {
+        println("Book titled '$title' by $author is created.")
+    }
+
+    constructor(title: String, author: String) : this(title, author, 500)
+}
+
+class RandomEmployeeGenerator(var maxSalary: Int, var minSalary: Int) {
+    private val names =
+        listOf("John", "Mary", "Ann", "Paul", "Jack", "Elizabeth")
+
     fun generateEmployee() =
-        Employee(names.random(),
-            Random.nextInt(from = minSalary, until = maxSalary))
+        Employee(
+            names.random(),
+            Random.nextInt(from = minSalary, until = maxSalary)
+        )
 }
 
 data class Employee(val name: String, var salary: Int)
